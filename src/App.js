541e631164d1f4633c6c156498151003bc21c382
rajat <rajat.k@sphinxworldbiz.co>
2023-08-11 17:35:31 +0530

    Reactjs Introduction finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import DemoComponent from './compos/DemoComponent';
+import ProfilePic from './images/bond.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+      <DemoComponent name="zena" age='35'>
+        <p>I am a web developer</p>
+        <p>I am from HP</p>
+        <img src={ProfilePic} alt=""></img>
+      </DemoComponent>
     </div>
   );
 }
---
+++ src/compos/DemoComponent.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const DemoComponent = ({name, age, children}) => {
+  const[title, setTitle] = useState('react js advanced');  
+  function clickHandler(){
+    console.log("My Name is Rajat");
+  }  
+  return (
+    <div>
+      <h1>{title}</h1>
+        <p>My Name is {name} and i am {age} old.</p>
+        <p>{children}</p>
+        <button onClick={()=>setTitle("React JS Beginner to Advanced")} >Change Title</button>
+        <button onClick={clickHandler}>Click</button>
+    </div>
+  )
+}
+
+export default DemoComponent
